c3e3f19a383501679154791b0869b52c045bdb07
System Administrator <zaagagon@gmail.com>
2022-03-01 23:44:13 -0500

    node js

---
+++ src/servidor/prueba.js
@@ -0,0 +1,10 @@
+var user = {
+    nombre: 'zag',
+    email: 'zag@datos.com',
+    edad: 23,
+    fechaRegistro: new Date(),
+    direccion: {
+                   ciudad: 'Miami',
+                   pais: 'USA' }
+}
+console.log(user);
