4a427369d4302405dd48b1472021fbe39f61f409
System Administrator <zaagagon@gmail.com>
2022-03-04 23:56:59 -0500

    let js

--- src/servidor/hello.js
+++ src/servidor/hello.js
@@ -3,6 +3,13 @@ function suma (x1,x2){
   return x1+x2
 }
 
-var a = "Bienvendio"
+function resta(r1,r2){
+    return r1-r2
+}
+//variable en javascript
+var a = "Bienvendo"
 console.log(a + "hola zag");
 console.log("suma de dos numeros :"+suma(2,3));
+console.log("digite su nombre");
+let nombre
+console.log("hola " + nombre);
