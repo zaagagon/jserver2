826f6201db7d13e718eb7473cee6108200c3c060
System Administrator <zaagagon@gmail.com>
2022-03-05 21:56:38 -0500

    console.log('\033[2J');

--- src/ejemplos_node/function_arrow.js
+++ src/ejemplos_node/function_arrow.js
@@ -1,7 +1,9 @@
 //function normal
 var saludar = function (nombre) {
+    
     return 'Hola ' + nombre;
   };
+  console.log('\033[2J'); 
   console.log( saludar('zag') ); //Muestra en consola Hola ZAG
-
   
+
