9885a64c18c738f555fd49f4c8697f0ee726d760
System Administrator <zaagagon@gmail.com>
2022-03-06 10:26:04 -0500

    suma2 js

---
+++ src/ejemplos_node/scripts/scripts/suma2.js
@@ -0,0 +1,7 @@
+//script ejecutado en el interprete de navegador
+var primero, segundo, resultado;
+primero = prompt ("Ingrese el primer número: ");
+segundo = prompt ("Ingrese el segundo número:");
+resultado = parseInt (primero) + parseInt (segundo);
+alert ("la suma de los dos numeros es :"+resultado);
+clear
