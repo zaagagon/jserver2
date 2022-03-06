f31cf7525cc3425382e3368d175cbefb9b6acd7d
System Administrator <zaagagon@gmail.com>
2022-03-06 10:16:52 -0500

    suma.js

--- src/ejemplos_node/scripts/scripts/main.js
+++ src/ejemplos_node/scripts/scripts/main.js
@@ -1,2 +1,5 @@
 const miTitulo = document.querySelector('h1');
 miTitulo.textContent = '¡Hola mundo de ZAG!';
+
+const subtitulo =document.querySelector('h2');
+subtitulo.textContent=' ** queryselector en javascript **';
---
+++ src/ejemplos_node/scripts/scripts/suma.js
@@ -0,0 +1,5 @@
+var primero, segundo, resultado;
+primero = prompt ("Ingrese el primer número: ");
+segundo = prompt ("Ingrese el segundo número:");
+resultado = parseInt (primero) + parseInt (segundo);
+alert (resultado);
