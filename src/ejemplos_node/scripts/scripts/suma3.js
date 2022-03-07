ca7845370b3541ef31514fe9abfd961a9aea18cf
System Administrator <zaagagon@gmail.com>
2022-03-06 23:40:30 -0500

    parseInty

--- src/ejemplos_node/scripts/scripts/suma3.js
+++ src/ejemplos_node/scripts/scripts/suma3.js
@@ -1,8 +1,13 @@
 //codigo js
+
 function sumar(n1,n2) {
     return n1+n2;
 
 }
 console.clear();
 console.log("Prueba zag");
-console.log("resultado suma : " + sumar(4,5));
+
+//llamdo funcion suma
+console.log("Resultado suma : " + sumar(4,5));
+var prueba = (n1,n2)=>sumar(7,7);
+console.log(parseInt (prueba));
