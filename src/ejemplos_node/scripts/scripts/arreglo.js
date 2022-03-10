9763d4d4a7248c16611373608cad79663c5a59c5
System Administrator <zaagagon@gmail.com>
2022-03-09 23:39:44 -0500

    array

--- src/ejemplos_node/scripts/scripts/arreglo.js
+++ src/ejemplos_node/scripts/scripts/arreglo.js
@@ -2,5 +2,8 @@
 console.clear();
 //otra manera de recorrer el arreglo
 let list=["pilas","mouse","key"];
-list.forEach((e,i)=>console.log("i= ",i,"list =",e,"tamaño del array :",list.length));
+
+//list.forEach((e,i)=>console.log("i= ",i,"list =",e,"tamaño del array :",list.length));
+list.forEach((e,i)=>console.log("i= ",i,"list =",e));
+
 console.log("tamaño del array list : ",list.length)
