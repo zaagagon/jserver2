fb8db4e8965efc013f191d1dbdb55f45ebae9f89
System Administrator <zaagagon@gmail.com>
2022-03-09 23:22:59 -0500

    comentario ejecucion

--- src/ejemplos_node/scripts/scripts/arreglo.js
+++ src/ejemplos_node/scripts/scripts/arreglo.js
@@ -1,4 +1,4 @@
 //
-
-["pilas","mouse","key"].forEach((e,i)=>
-                                        console.log("i= ",i,"list =",e));
+console.clear();
+//otra manera de recorrer el arreglo
+["pilas","mouse","key"].forEach((e,i)=>console.log("i= ",i,"list =",e));
